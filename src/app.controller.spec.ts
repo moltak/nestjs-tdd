7694157883dfc4f86ee5fcc8616fce9565075c6d
Clark <clark@contxts.io>
2022-02-06 15:20:46 +0900

    step 4. 조금 다른 성격의 Test 코드 작성

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -23,6 +24,19 @@ describe('AppController', () => {
       // then
       expect(user).toBeTruthy();
       expect(user.id).toEqual(Number.parseInt(userId, 10));
+    });
+
+    it('should return NotFoundException', async () => {
+      expect.assertions(1);
+      // given
+      const userId = '1';
+      // when
+      // then
+      try {
+        await appController.getUser(userId);
+      } catch (err) {
+        expect(err).toEqual(new NotFoundException());
+      }
     });
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UserDto } from './dto/user.dto';
 
